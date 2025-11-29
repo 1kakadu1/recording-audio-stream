e95ceeae633290380e8a7a0654e06c68640a63e5
Anton <1kakady@gmail.com>
2025-11-29 19:40:10 +0300

    fix git bug

---
+++ front/src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AboutPage } from "./pages/about";
+import { HomePage } from "./pages/home";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
